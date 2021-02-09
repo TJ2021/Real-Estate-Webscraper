72d6eb1aa9bf77ba13fc68b3b1b0441d52728cb2
TJ2021 <treesaj18@gmail.com>
2021-02-09 15:50:33 -0600

    Crawling through webpages

--- webscraper.py
+++ webscraper.py
@@ -6,51 +6,51 @@ from bs4 import BeautifulSoup
 
 import pandas
 
-#Response object will be returned. Response object contains server's response to an HTTP request
-response = requests.get('https://www.century21.com/real-estate/round-rock-tx/LCTXROUNDROCK/?')
-#content of the response
-c = response.content
-#pull data from html file
-soup = BeautifulSoup(c,'html.parser')
-
 l = []
-properties = soup.find_all('div',{'class':'property-card-primary-info'})
-for property in properties:
-    d = {}
-    try:
-        d['Price'] = property.find('a',{'class':'listing-price'}).text.replace('\n','').replace(' ','')
-    except:
-        d['Price'] = 'None'
+base_url = 'https://www.century21.com/real-estate/round-rock-tx/LCTXROUNDROCK/?p='
+#Response object will be returned. Response object contains server's response to an HTTP request
+for page in range(1,3):
+    response = requests.get(base_url+str(page))
+    #content of the response
+    c = response.content
+    #pull data from html file
+    soup = BeautifulSoup(c,'html.parser')
+    properties = soup.find_all('div',{'class':'property-card-primary-info'})
+    for property in properties:
+        d = {}
+        try:
+            d['Price'] = property.find('a',{'class':'listing-price'}).text.replace('\n','').replace(' ','')
+        except:
+            d['Price'] = 'None'
 
-    try:
-        d['Beds'] = property.find('div',{'class':'property-beds'}).text.replace('\n','')
-    except:
-        d['Beds'] = 'None'
+        try:
+            d['Beds'] = property.find('div',{'class':'property-beds'}).text.replace('\n','')
+        except:
+            d['Beds'] = 'None'
 
-    try:
-        d['Half-baths'] = property.find('div',{'class':'property-half-baths'}).text.replace('\n','')
-    except:
-        d['Half-baths'] = 'None'
+        try:
+            d['Half-baths'] = property.find('div',{'class':'property-half-baths'}).text.replace('\n','')
+        except:
+            d['Half-baths'] = 'None'
                 
-    try:
-        d['Sqft'] = property.find('div',{'class':'property-sqft'}).text.replace('\n','')
-    except:
-        d['Sqft'] = 'None'
+        try:
+            d['Sqft'] = property.find('div',{'class':'property-sqft'}).text.replace('\n','')
+        except:
+            d['Sqft'] = 'None'
                 
-    try:
-        d['Address'] = property.find('div',{'class':'property-address'}).text.replace('   ','').replace('\n','')
-    except:
-        d['Address'] = 'None'
+        try:
+            d['Address'] = property.find('div',{'class':'property-address'}).text.replace('   ','').replace('\n','')
+        except:
+            d['Address'] = 'None'
                 
-    try:
-        d['City'] = property.find('div',{'class':'property-city'}).text.replace('   ','').replace('\n','')
-    except:
-        d['City'] = 'None'
+        try:
+            d['City'] = property.find('div',{'class':'property-city'}).text.replace('   ','').replace('\n','')
+        except:
+            d['City'] = 'None'
                 
-    l.append(d)
-
-df = pandas.DataFrame(l)
-#save extracted data in csv file
-df.to_csv('output.csv')
+        l.append(d)
 
 
+    df = pandas.DataFrame(l)
+    #save extracted data in csv file
+    df.to_csv('output.csv')
